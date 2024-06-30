4dead813e9fa5fe8aef890b2f2843ed5ccbba4eb
mauricecliff <coderzden2023@gmail.com>
2024-06-30 04:24:53 +0100

    added side bar, notification and nivo chart to the dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SideBar from "@/components/custom/SideBar";
+import Header from "@/components/custom/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex items-start justify-between`}>
+        <SideBar />
+        <main className="w-full h-full">
+        <Header />
+        {children}
+        </main>
+        </body>
     </html>
   );
 }
